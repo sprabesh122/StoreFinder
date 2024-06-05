ed9a9bb1e71ae10ff69d880e7dd80b6c6cb478a9
Rajshree326 <2020ugcs046@nitjsr.ac.in>
2024-06-05 23:06:22 +0530

    changes for admin page

--- src/main/java/com/prabeshcodes/student/controller/CategoryController.java
+++ src/main/java/com/prabeshcodes/student/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.prabeshcodes.student.controller;
 
+import com.prabeshcodes.student.dtos.CategoryResponse;
 import com.prabeshcodes.student.model.Category;
+import com.prabeshcodes.student.repository.CategoryRepository;
 import com.prabeshcodes.student.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     // Logic to get category by ID
     @GetMapping("/{id}")
@@ -21,5 +29,18 @@ public class CategoryController {
     @PostMapping("/add")
     public Category addCategory(@RequestBody Category category){
         return categoryService.addCategory(category);
+    }
+
+    @GetMapping("/getAll")
+    public List<CategoryResponse> getCategory(){
+        List<CategoryResponse> result = new ArrayList<>();
+        List<Category> categories = categoryRepository.findAll();
+        for(Category category:categories){
+            CategoryResponse categoryResponse = new CategoryResponse();
+            categoryResponse.setId(category.getId());
+            categoryResponse.setName(category.getName());
+            result.add(categoryResponse);
+        }
+        return result;
     }
 }
